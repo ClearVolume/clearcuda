43d22a4c830dc6170e22a8bf7d08999bd16ff61e
Loic Alain Royer <royerloic@gmail.com>
2014-12-03 20:14:41 +0100

    fixed CUDA detection, now it actually runs a test kernel to be sure

--- src/clearcuda/CudaAvailability.java
+++ src/clearcuda/CudaAvailability.java
@@ -1,5 +1,7 @@
 package clearcuda;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 
 import clearcuda.utils.NVCC;
@@ -10,13 +12,53 @@ public class CudaAvailability
 	{
 		try
 		{
-			boolean lDeviceAvailable = CudaDevice.isCudaDeviceAvailable();
-			boolean lCompilerIsAvailable = isCudaCompilerAvailable();
-			return lDeviceAvailable && lCompilerIsAvailable;
+			return CudaDevice.isCudaDeviceAvailable() && isCudaCompilerAvailable()
+							&& doesCudaWorks();
 		}
 		catch (Throwable e)
 		{
+			System.err.println(e.getLocalizedMessage());
 			System.out.println("CUDA NOT AVAILABLE");
+			return false;
+		}
+	}
+
+	private static boolean doesCudaWorks()
+	{
+		try
+		{
+			long lChannels = 2;
+			long lWidth = 128;
+			long lHeight = 1;
+			long lDepth = 1;
+			int lLength = (int) (lChannels * lWidth * lHeight * lDepth);
+
+			try (CudaDevice lCudaDevice = new CudaDevice(0);
+					CudaContext lCudaContext = new CudaContext(	lCudaDevice,
+																											false);
+					CudaArray lCudaArray = new CudaArray(	lChannels,
+																								lWidth,
+																								lHeight,
+																								lDepth,
+																								4,
+																								true,
+																								false,
+																								true))
+			{
+				float[] lFloatsIn = new float[lLength];
+				lFloatsIn[lLength / 2] = 123;
+				lCudaArray.copyFrom(lFloatsIn, true);
+
+				float[] lFloatsOut = new float[lLength];
+				lCudaArray.copyTo(lFloatsOut, true);
+				assertEquals(123, lFloatsOut[lLength / 2], 0);
+			}
+			return true;
+		}
+		catch (Throwable e)
+		{
+			System.err.println(e.getLocalizedMessage());
+			System.err.println("CUDA KERNEL TEST CALL FAILED!");
 			return false;
 		}
 	}
@@ -31,6 +73,7 @@ public class CudaAvailability
 		}
 		catch (Throwable e)
 		{
+			System.err.println(e.getLocalizedMessage());
 			System.out.println("CUDA COMPILER NOT AVAILABLE");
 			lCompilerIsAvailable = false;
 		}
